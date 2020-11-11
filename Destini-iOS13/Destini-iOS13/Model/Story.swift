dfc0a521b5aa6afd05a4b5e138d10290563f0407
LEmilio14 <47877642+LEmilio14@users.noreply.github.com>
2020-11-11 15:00:31 -0800

    Add files via upload

---
+++ Destini-iOS13/Destini-iOS13/Model/Story.swift
@@ -0,0 +1,25 @@
+//
+//  Sotry.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+    
+    init(t: String, c1: String, c1D: Int, c2: String, c2D: Int){
+        title = t
+        choice1 = c1
+        choice1Destination = c1D
+        choice2 = c2
+        choice2Destination = c2D
+    }
+}
